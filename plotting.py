c5745ce588ec49d0818c4ebcaa15136d35aa4c25
gunnarthorsteins <gunnarthorsteins@outlook.com>
2021-09-10 00:01:03 -0400

    dev1 closed

--- plotting.py
+++ plotting.py
@@ -1,4 +1,4 @@
-
+import json
 from bokeh.models import ColumnDataSource, LayoutDOM
 from bokeh.core.properties import Instance, String
 from bokeh.models import ColumnDataSource, LayoutDOM
@@ -6,53 +6,61 @@ from bokeh.util.compiler import TypeScript
 from bokeh.plotting import figure, show
 
 
-def plot_2d(self):
+class Plotting:
+    def __init__(self):
+        pass
 
-    self.split_curve()
+    # def plot_2d(self):
 
-    # y-axis is reversed by reversing the axis range
-    self.fig2D = figure(title="2D Trajectory",
-                        x_axis_label='Horizontal Throw [m]',
-                        y_axis_label='Vert. Depth [m]',
-                        x_range=(-50, max(self.r) + 100),
-                        y_range=(max(self.z) + 100, -50 - self.Z))
+    #     self.split_curve()
 
-    self.source2D = ColumnDataSource(data=dict(x=self.r, y=self.z))
-    self.fig2D.line(x='x', y='y', data_source=self.source2D)
+    #     # y-axis is reversed by reversing the axis range
+    #     self.fig2D = figure(title="2D Trajectory",
+    #                         x_axis_label='Horizontal Throw [m]',
+    #                         y_axis_label='Vert. Depth [m]',
+    #                         x_range=(-50, max(self.r) + 100),
+    #                         y_range=(max(self.z) + 100, -50 - self.Z))
+
+    #     self.source2D = ColumnDataSource(data=dict(x=self.r, y=self.z))
+    #     self.fig2D.line(x='x', y='y', data_source=self.source2D)
+
+    # def plot_3d(self):
+
+    #     self.fig3D = px.scatter_3d(title="3D Trajectory",
+    #                                x_axis_label='Horizontal Throw [m]',
+    #                                y_axis_label='Vert. Depth [m]',
+    #                                x_range=(-50, max(r) + 100),
+    #                                y_range=(max(z) + 100, -50 - self.Z))
+    #     ax3D.plot3D(x[:self.split+1], y[:self.split+1],
+    #                 self.z[:self.split+1], 'k')
+    #     ax3D.plot3D(x[self.split:], y[self.split:], self.z[self.split:], 'r')
+    #     ax3D.set_box_aspect([1, 1, 1])
+    #     ax3D.invert_zaxis()
+    #     # show(row(self.fig2D, self.fig3D))
+
+    def plot_surface(self):
+        class Surface3d(LayoutDOM):
+            with open('vis.ts') as tsFile:
+                TS_CODE = tsFile.read()
+
+            __implementation__ = TypeScript(TS_CODE)
+            data_source = Instance(ColumnDataSource)
+
+            x = String
+            y = String
+            z = String
 
 
-def plot_3d(self):
+        location = 'Svartsengi'
+        with open(f'data/{location}.json') as f:
+            dict_ = json.load(f)
+        print(dict_)
+        source3D = ColumnDataSource(data=dict_)
 
-    self.fig3D = px.scatter_3d(title="3D Trajectory",
-                               x_axis_label='Horizontal Throw [m]',
-                               y_axis_label='Vert. Depth [m]',
-                               x_range=(-50, max(r) + 100),
-                               y_range=(max(z) + 100, -50 - self.Z))
-    ax3D.plot3D(x[:self.split+1], y[:self.split+1],
-                self.z[:self.split+1], 'k')
-    ax3D.plot3D(x[self.split:], y[self.split:], self.z[self.split:], 'r')
-    ax3D.set_box_aspect([1, 1, 1])
-    ax3D.invert_zaxis()
-    # show(row(self.fig2D, self.fig3D))
+        surface = Surface3d(
+            x='x', y='y', z='z', data_source=source3D)
+        show(surface)  # saves the figure as elevation.html
 
-
-def plot_surface(self):
-
-    source3D = ColumnDataSource(data=dict(x=self.X,
-                                          y=self.Y,
-                                          z=self.Z))
-
-    class Surface3d(LayoutDOM):
-        with open('vis.ts') as tsFile:
-            TS_CODE = tsFile.read()
-
-        __implementation__ = TypeScript(TS_CODE)
-        data_source = Instance(ColumnDataSource)
-
-        x = String
-        y = String
-        z = String
-
-    surface = Surface3d(
-        x='x', y='y', z='z', data_source=source3D)
-    show(surface)  # saves the figure as elevation.html
+if __name__ == "__main__":
+    plotting = Plotting()
+    plotting.plot_surface() 
--- trajectory.py
+++ trajectory.py
@@ -1,114 +1,209 @@
+import json
 import numpy as np
 import warnings
 
+import matplotlib.pyplot as plt
+
+
+M_TO_DEG = 1.11e5  # m to deg on Earth's surface
+
 
 class Trajectory2d:
-    def _cosd(deg):
+
+    '''Three legs:
+    1) Down to KOP
+    2) Build-up
+    3) Last leg to well bottom (straight)
+    '''
+
+    def __init__(self):
+
+        with open('data/defaults.json') as json_file:
+            default_values = json.load(json_file)
+
+        self.lon = default_values["lon"]
+        self.lat = default_values["lat"]
+        self.mmd = default_values["mmd"]
+        self.dip = default_values["dip"]
+        self.Z = default_values["Z"]
+        self.az = default_values["az"]
+        self.cd = default_values["cd"]
+        self.kop = default_values["kop"]
+        self.bu = default_values["bu"]
+
+        self.assemble()
+
+    def _cosd(self, deg):
         return np.cos(np.deg2rad(deg))
 
-    def _sind(deg):
+    def _sind(self, deg):
         return np.sin(np.deg2rad(deg))
 
-    def _tand(deg):
-        return np.tan(np.deg2rad(deg))
+    def _get_index(self, a, b):
+        return sum(a < b)
 
-    def _casing_split(self):        # Determine casing TVD to split curve
-        """Splits the curve above/below casing for plot coloring"""
+    def _casing_split(self):
+        """Splits the curve at end of casing for plot coloring
+
+        Returns:
+            split (int): Index denoting where to split r & z
+        """
+
+        shifted_cd = self.cd - self.Z
 
         if self.cd <= self.kop:
-            self.split = sum(self.z < (self.cd - self.Z))
-        elif self.kop < self.cd < len_2[-1]:
-            self.split = 50 + sum(len_2 < (self.cd - self.Z))
-        elif len_2[-1] < self.cd:
-            self.split = 50 + len(z2) + sum(len_3 < (self.cd - self.Z))
+            split_index = self._get_index(self.z, shifted_cd)
+        elif self.kop < self.cd < self.len_buildup[-1]:
+            split_index = self._get_index(self.len_buildup, shifted_cd)
+        elif self.len_buildup[-1] < self.cd:
+            # NOTE Used to have +50 here, don't know why
+            split_index = len(self.len_buildup) + \
+                self._get_index(self.len_slanted, shifted_cd)
         else:
             warnings.warn('Check casing block')
 
-    def __init__(self):
-        pass
+        return split_index
 
-    def trajectory_2d(self):
-        '''Three legs:
-        1) Down to KOP
-        2) Build-up
-        3) Last leg to well bottom (straight)
-        '''
+    def vertical_leg(self):
+        """First leg - a vertical well segment
 
-        def first_leg():
-            """Creates a vertical well segment"""
+        Returns:
+            r_vertical (1D array): Horizontal displacement (all zeros)
+            z_vertical (1D array): Vertical displacement linspace
+        """
 
-            z = np.linspace(-self.Z, self.kop - self.Z)
-            r = np.zeros_like(z)
+        z_vertical = np.linspace(-self.Z, self.kop - self.Z)
+        r_vertical = np.zeros_like(z_vertical)
 
-            return r, z
+        return r_vertical, z_vertical
 
-        # Vertical
+    def buildup_leg(self, end_z_vertical):
+        """Second leg - gradual incline buildup.
+
+        Returns:
+            r_buildup (1D array): Horizontal displacement, perpendicular to azimuth
+            z_vertical (1D array): Vertical displacement
+        """
+
+        r_buildup = np.zeros(self.dip - 1)
+        z_buildup = np.zeros(self.dip - 1)
+        self.len_buildup = np.zeros(self.dip - 1)
+
+        z_buildup[0] = end_z_vertical
+        self.len_buildup[0] = end_z_vertical
+        for i in range(1, self.dip - 1):
+            r_buildup[i] = r_buildup[i-1] + self._sind(i) / self.bu
+            z_buildup[i] = z_buildup[i-1] + self._cosd(i) / self.bu
+            self.len_buildup[i] = self.len_buildup[i-1] + 1 / self.bu
+
+        return r_buildup, z_buildup
+
+    def slanted_leg(self, end_r_buildup, end_z_buildup):
+        """Third and last leg - straight slanted.
+
+        Args:
+            end_r_buildup (float): Last r val of previous leg
+            end_z_buildup (float): Last z val of previous leg
+
+        Returns:
+            r_slanted (1D array): Horizontal displacement, perpendicular to azimuth
+            z_slanted (1D array): Vertical displacement
+        """
+
+        L2 = self.mmd - self.kop - self.dip / self.bu
+        r_slanted = np.linspace(end_r_buildup, L2 * self._sind(self.dip))
+        vert = end_z_buildup + np.linspace(0, L2 * self._cosd(self.dip))
+        p = np.polyfit(r_slanted, vert, 1)
+        z_zlanted = np.polyval(p, r_slanted)
+        self.len_slanted = z_zlanted / self._cosd(self.dip)
+
+        return r_slanted, z_zlanted
+
+    def assemble(self):
+        self.r, self.z = self.vertical_leg()
         if not self.kop:
-            self.r, self.z = first_leg()
-
             return
 
-        # Directional
-        # First leg
-        r1, z1 = first_leg()
+        r2, z2 = self.buildup_leg(self.z[-1])
+        r3, z3 = self.slanted_leg(r2[-1], z2[-1])
 
-        # Second leg - The curved segment
-        r2 = np.zeros(self.dip - 1)
-        z2 = r2
-        len_2 = r2
-        z2[0] = z1[-1]
-        len_2[0] = z1[-1]
-        for i in range(1, self.dip - 1):
-            r2[i] = r2[i-1] + self._sind(i) / self.bu
-            z2[i] = z2[i-1] + self._cosd(i) / self.bu
-            len_2[i] = len_2[i-1] + 1 / self.bu
+        self.r = np.concatenate((self.r, r2, r3))
+        self.z = np.concatenate((self.z, z2, z3))
+        split_index = self._casing_split()
 
-        # Third leg - The last one
-        L2 = self.mmd - self.kop - self.dip / self.bu
-        r3 = np.linspace(
-            r2[-1], L2 * self._sind(self.dip)
-        )
-        vert = z2[-1] + np.linspace(0, L2 * self._cosd(self.dip))
-        p = np.polyfit(r3, vert, 1)
-        z3 = np.polyval(p, r3)
-        len_3 = z3 / self._cosd(self.dip)
-
-        self.r = np.concatenate((r1, r2, r3))
-        self.z = np.concatenate((z1, z2, z3))
+    def test_plot(self):
+        plt.plot(self.r, self.z)
 
 
 class Trajectory3d(Trajectory2d):
     def __init__(self):
-        pass
+        super().__init__()
 
+    def _tand(self, deg):
+        return np.tan(np.deg2rad(deg))
 
-    def trajectory_3d(self):
+    def _delta(self, r):
+        """Forks horizontal displacement r into x and y components using the azimuth.
 
-        def delta(r):
+        Args:
+            r (float): r component (inherited from super)
 
-            delta_y = self.r / np.sqrt(self._tand(self.az)**2 + 1)
-            delta_x = delta_y * self._tand(self.az)
+        Returns:
+            delta_x (float): east/westbound increment
+            delta_y (float): north/southbound increment
+        """
 
-            return delta_x, delta_y
+        delta_y = r / np.sqrt(self._tand(self.az)**2 + 1)
+        delta_x = delta_y * self._tand(self.az)
+
+        delta_y /= M_TO_DEG
+        # m to deg is lat dependent for lon
+        delta_x *= self._cosd(self.lat) / M_TO_DEG
+
+        return delta_x, delta_y
+
+    def fork_r(self):
+        """Forks vector r into x and y components.
+
+        Returns:
+            x (1D array): east/westbound component
+            y (1D array): north/southbound component 
+        """
 
         x = self.lon + np.zeros(len(self.r))
         y = self.lat + np.zeros(len(self.r))
-        m_to_deg = 1.11e5  # m to deg on Earth's surface
+
+        # Special cases -> tand(az) = inf
+        if self.az == 90 or self.az == 270:
+            x += self.r
+            return x, y
 
         for i, r in enumerate(self.r):
-            # Accounting for special cases -> tand(az) = inf
-            if self.az == 90 or self.az == 270:
-                delta_y = 0
-                delta_x = self.r[-1]
-            else:
-                delta_x, delta_y = delta(r)
-
-            delta_y /= m_to_deg
-            # m to deg is lat dependent for lon
-            delta_x *= self._cosd(self.lat) / m_to_deg
-            if self.az <= 90 or self.az > 270:
-                y[i] += delta_y
-                x[i] += delta_x
-            else:
+            delta_x, delta_y = self._delta(r)
+            if 90 < self.az <= 270:
                 y[i] -= delta_y
                 x[i] -= delta_x
+            else:
+                y[i] += delta_y
+                x[i] += delta_x
+
+        return x, y
+
+    def run(self):
+        self.x, self.y = self.fork_r()
+
+    def test_plot(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(self.x, self.y, self.z)
+        fig.show()
+
+
+if __name__ == '__main__':
+
+    # traj2d = Trajectory2d()
+    # traj2d.assemble()
+    # traj2d.test_plot()
+    traj3d = Trajectory3d()
+    traj3d.run()
+    traj3d.test_plot()
